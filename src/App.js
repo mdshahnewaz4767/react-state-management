1bfdb7fd2529b4e1589581adf1d9bebc1f61d1a1
shahnewaz601 <shahnewaz601@gmail.com>
2021-03-07 01:58:52 +0600

    Share data to grandchildren

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import Shipment from './components/Shipment/Shipment';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Header count={count} setCount={setCount}></Header>
       <Home count={count} setCount={setCount}></Home>
+      <Shipment></Shipment>
     </div>
   );
 }
