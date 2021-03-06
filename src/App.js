ada2f9b3aa112b9aa9dbc596ea511c5d445c522f
shahnewaz601 <shahnewaz601@gmail.com>
2021-03-06 23:53:56 +0600

    Share state with sibling component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <div>
       <Header count={count} setCount={setCount}></Header>
+      <Home count={count} setCount={setCount}></Home>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ const Header = (props) => {
     const {count, setCount} = props;
     return (
         <div>
-            <h1>This is header: {count}</h1>
+            <h1>This is Header: {count}</h1>
             <button onClick={() => setCount(count + 1)}>Increment</button>
         </div>
     );
