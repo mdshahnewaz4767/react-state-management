768af22a2ff5849ca44fb44e8a368f602bbde049
shahnewaz601 <shahnewaz601@gmail.com>
2021-03-07 23:23:00 +0600

    Filter data based on dynamic context change

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { CategoryContext } from '../../App';
 import CategoryDetails from '../CategoryDetails/CategoryDetails';
+
+
+const allProducts = [{id: '0', name: 'Asus', category: 'Laptop'}, {id: '1', name: 'Lenovo', category: 'Laptop'}, {id: '3',name: 'Dell', category: 'Laptop'}, {id: '4', name: 'Oneplus', category: 'Mobile'}, {id: '5', name: 'Nokia', category: 'Mobile'}, {id: '6', name: 'Apple', category: 'Mobile'}, {id: '7', name: 'Canon', category: 'Camera'}, {id: '8', name: 'Nikon', category: 'Camera'}, {id: 'fa-rotate-90', name: 'Sony', category: 'Camera'}];
 
 const Categories = () => {
 
+    const [category] = useContext(CategoryContext);
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        console.log({category});
+        const matchedProducts = allProducts.filter(product => product.category === category && category.toLowerCase());
+        setProducts(matchedProducts);
+    }, [category])
     return (
         <div>
-            <h2>Select your category</h2>
-            <CategoryDetails></CategoryDetails>
+            <h2>Select your category: {category}</h2>
+            {
+                products.map(product => <CategoryDetails product={product} key={product.id}></CategoryDetails>)
+            }
         </div>
     );
 };
--- src/components/CategoryDetails/CategoryDetails.js
+++ src/components/CategoryDetails/CategoryDetails.js
@@ -1,12 +1,12 @@
-import React, { useContext } from 'react';
-import { CategoryContext } from '../../App';
+import React from 'react';
 
-const CategoryDetails = () => {
-    const category = useContext(CategoryContext);
+const CategoryDetails = (props) => {
+    console.log(props);
+    const {name} = props.product;
     return (
         <div>
             <h4>This is your Category Details</h4>
-            <h6>Selected Category: {category}</h6>
+            <h6>Selected Category: {name}</h6>
         </div>
     );
 };
